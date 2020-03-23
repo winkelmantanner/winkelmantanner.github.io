3bc2295d3a7c4e26a5ab6b4552a039d25fad22f6
Tanner Winkelman <winkelmantanner@gmail.com>
2020-03-23 17:16:18 -0500

    Use React to do the astro tools.

---
+++ astro-tools/precache-manifest.58d711cc0696374cd252d100756f96bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ce3ee08ebe24b233a9c078b2225391",
+    "url": "/astro-tools/index.html"
+  },
+  {
+    "revision": "ff40c9cdd8e384fb2753",
+    "url": "/astro-tools/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "97debe81c4ff6f2ed0a7",
+    "url": "/astro-tools/static/js/2.8a1446b4.chunk.js"
+  },
+  {
+    "revision": "395cb1218ce0f7ec05972455cf375260",
+    "url": "/astro-tools/static/js/2.8a1446b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff40c9cdd8e384fb2753",
+    "url": "/astro-tools/static/js/main.4a79cb35.chunk.js"
+  },
+  {
+    "revision": "98ccd35a0df65f82ecf4",
+    "url": "/astro-tools/static/js/runtime-main.06328a1e.js"
+  }
+]);
