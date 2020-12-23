c7bf6ce6799ee50b628de3593bd1c059c33fc824
Tanner Winkelman <winkelmantanner@gmail.com>
2020-12-22 18:18:29 -0600

    Work on astro-tools

--- astro-tools/precache-manifest.afdbb197086d04cfb5fa57864037f36d.js
+++ astro-tools/precache-manifest.afdbb197086d04cfb5fa57864037f36d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "36f0e64f2343ca96dba429c40d8e4df3",
+    "revision": "423eee7e3f13301c268ab37412c08624",
     "url": "/astro-tools/index.html"
   },
   {
-    "revision": "5967e501db8fae845ea8",
+    "revision": "51af2f3f4ae5133dc09b",
     "url": "/astro-tools/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/astro-tools/static/js/2.890f9d0b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5967e501db8fae845ea8",
-    "url": "/astro-tools/static/js/main.40f33132.chunk.js"
+    "revision": "51af2f3f4ae5133dc09b",
+    "url": "/astro-tools/static/js/main.ab5cea6d.chunk.js"
   },
   {
     "revision": "dd574ad454613c9ade73",
