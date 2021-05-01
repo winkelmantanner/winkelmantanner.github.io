bdb9d265035592ca1670aceb47d736cf99cb9c16
Tanner Winkelman <winkelmantanner@gmail.com>
2021-05-01 11:06:31 -0500

    Update astro-tools with a calculation fix in the Polaris page.

--- astro-tools/precache-manifest.ace3eda90f2696f8d0bdc430e598fe15.js
+++ astro-tools/precache-manifest.ace3eda90f2696f8d0bdc430e598fe15.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb34b140a4171030caadc11ebb47c9c5",
+    "revision": "d346c3c35c68f9d1588638cd676954a0",
     "url": "/astro-tools/index.html"
   },
   {
-    "revision": "8da4f73df23ddc00da3d",
+    "revision": "1fd5c257e8e334467d31",
     "url": "/astro-tools/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/astro-tools/static/js/2.2d5b3650.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8da4f73df23ddc00da3d",
-    "url": "/astro-tools/static/js/main.227ef0dd.chunk.js"
+    "revision": "1fd5c257e8e334467d31",
+    "url": "/astro-tools/static/js/main.8ee98ad2.chunk.js"
   },
   {
     "revision": "dd574ad454613c9ade73",
